e0ded68fbb868b55048a0c650bca735a91d457c6
pritishraj01 <pritish499@gmail.com>
2026-01-03 18:11:31 +0530

    cookies fix

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -34,7 +34,7 @@ export const signup = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: true,
-            sameSite: "none",
+            sameSite: "None",
             maxAge: 365 * 24 * 60 * 60 * 1000
         })
 
@@ -66,7 +66,7 @@ export const login = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: true,
-            sameSite: "none",
+            sameSite: "None",
             maxAge: 365 * 24 * 60 * 60 * 1000
         })
 
@@ -81,7 +81,7 @@ export const logout = async (req, res) => {
      res.clearCookie("token", {
         httpOnly: true,
         secure: true,
-        sameSite: "none",
+        sameSite: "None",
     })
     
     return res.status(200).json({message:"Logout successfully"})
